239f1c2f7193aae45f9efc17f0f722d405dabb01
Wesley Monaro <wesley@elefanteverde.com.br>
2016-08-15 14:09:38 -0300

    create project

---
+++ ntask-api/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import consign from "consign";
+const app = express();
+consign()
+    .include("models")
+    .then("libs/middlewares.js")
+    .then("routes")
+    .then("libs/boot.js")
+    .into(app);
---
+++ ntask-api/routes/index.js
@@ -0,0 +1,11 @@
+module.exports = app => {
+    app.get("/tasks", (req, res) => {
+        res.json({
+            tasks: [{
+                title: "Fazer compras"
+            }, {
+                title: "Consertar o pc"
+            }, ]
+        });
+    });
+};
