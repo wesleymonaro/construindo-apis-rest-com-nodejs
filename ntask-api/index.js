c67b73c0ceebef5bd61f4a976871216362b81029
Wesley Monaro <wesley@elefanteverde.com.br>
2016-08-15 17:09:52 -0300

    add sequelize

--- ntask-api/index.js
+++ ntask-api/index.js
@@ -2,8 +2,9 @@ import express from "express";
 import consign from "consign";
 const app = express();
 consign()
-    .include("models")
-    .then("libs/middlewares.js")
-    .then("routes")
-    .then("libs/boot.js")
-    .into(app);
+.include("libs/config.js")
+.then("db.js")
+.then("libs/middlewares.js")
+.then("routes")
+.then("libs/boot.js")
+.into(app);
